7afdadce14e1164870c22c1f7dc8716894b0f573
Clement Bruno <clmnt.bruno@gmail.com>
2016-11-21 20:14:24 +0100

    sign in and sign up forms ok

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,2 +1,25 @@
 class MeetingsController < ApplicationController
+  def index
+    @guest_meetings = Meeting.where(user_id == current_user.id)
+    @host_meetings = Meeting.where(user_id == meals.user)
+  end
+
+  def approve
+    @host_meeting = Meeting.where(meeting_params)
+    @host_meeting.status = "Approved"
+    render "meetings/index"
+  end
+
+  def deny
+    @host_meeting = Meeting.find(meeting_params)
+    @host_meeting.status = "Denied"
+    render "meetings/index"
+  end
+
+  private
+
+  def meeting_params
+    params.require(:meeting).permit(:status)
+  end
 end
+
