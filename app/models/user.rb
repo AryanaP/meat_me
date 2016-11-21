b8d1469385abbbc93ee1f4960baf467098c6e9aa
Aryana Pezé <aryana.ravari@gmail.com>
2016-11-21 19:16:54 +0100

    done controller, model, view User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :meals, dependent: :destroy
+  has_many :dates
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, presence:true
+  validates :last_name, presence:true
+  validates :gender, presence:true
+  # validates :photo, presence:true
+  validates :description, presence:true
+
+
 end
