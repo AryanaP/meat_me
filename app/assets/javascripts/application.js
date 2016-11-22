499d57febc869fafeaf8ed36f40c2d8663e84fad
Aryana Pezé <aryana.ravari@gmail.com>
2016-11-22 12:53:37 +0100

    flash alerts functional

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,9 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function() {
+  $('.alert').delay(500).fadeIn('normal', function() {
+    $(this).delay(1000).fadeOut();
+  });
+});
