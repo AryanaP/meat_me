adeeb24a8427d5d1646669ada866cb1c97fe16e3
Mamoun Benmeradi <mamoun@benmeradi.com>
2016-11-22 11:42:24 +0100

    Add footer, update Navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @meals = Meal.all
-    @last_meals = @meals.last(3)
+    @last_meals =Meal.last(3)
   end
 end
