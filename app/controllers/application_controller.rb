136b95933c0d86645c92ab626c27d9f2f597417f
Aryana Pezé <aryana.ravari@gmail.com>
2016-11-24 18:20:36 +0100

    signin/signup renvoie sur la page en cours

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  before_filter :store_current_location, :unless => :devise_controller?
+
+
+  def store_current_location
+    store_location_for(:user, request.url)
+  end
+
+  def after_sign_in_path_for(resource)
+    sign_in_url = new_user_session_url
+    if request.referer == sign_in_url
+      super
+    else
+      stored_location_for(resource) || request.referer || root_path
+    end
+  end
+
+  def after_sign_out_path_for(resource)
+    request.referrer || root_path
+  end
+
 end
+
