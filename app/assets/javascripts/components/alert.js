8b1d69b0ce411ca28c3e55b3caac8df21bc23890
Mamoun Benmeradi <mamoun@benmeradi.com>
2016-11-24 20:36:32 +0100

    fix review

---
+++ app/assets/javascripts/components/alert.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+
+// ALERTES
+  $('.alert').delay(500).fadeIn('normal', function() {
+    $(this).delay(1000).fadeOut();
+  });
+
+});
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,20 @@
 class ReviewsController < ApplicationController
-  before_action :set_user
+  # before_action :set_user
 
     def new
+      @user = Meeting.find(params[:meeting_id]).meal.user
+      @meal = Meeting.find(params[:meeting_id]).meal
+      @meeting = Meeting.find(params[:meeting_id])
       @review = Review.new
     end
 
     def create
-      @review = Review.new(review_params)
-      @review.user = @user
+      @review = Review.new(content: params["review"][:content], star: params["review"][:star], meeting_id: params[:meeting_id], user_id: Meeting.find(params[:meeting_id]).meal.user.id)
+      @user = Meal.find(params[:meal_id]).user
       if @review.save
         redirect_to user_path(@user)
       else
+        # Bug ici
         render :new
       end
     end
@@ -21,7 +25,7 @@ class ReviewsController < ApplicationController
       params.require(:review).permit(:content, :star)
     end
 
-    def set_user
-      @user = User.find(params[:user_id, :meeting_id])
-    end
+    # def set_user
+    #   @user = User.find(params[:user_id, :meeting_id])
+    # end
   end
